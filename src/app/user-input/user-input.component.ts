10de71737066f742cbf22629568496f1a9e73c92
NS918326 <Navrajgaltee@outlook.com>
2024-09-23 16:46:27 -0400

    ADD: Calculating Investment Results functionality implemented.

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,12 +9,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  initialInvestment = "0";
-  annualInvestment = "0";
-  expectedReturn = "5";
-  duration = "10";
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '10';
+  @Output() calculate = new EventEmitter<{
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+  }>();
 
   onSubmit = () => {
-    console.log(this.initialInvestment, this.annualInvestment, this.expectedReturn, this.duration);
-  }
+    this.calculate.emit({
+      initialInvestment: +this.initialInvestment,
+      annualInvestment: +this.annualInvestment,
+      expectedReturn: +this.expectedReturn,
+      duration: +this.annualInvestment,
+    });
+  };
 }
